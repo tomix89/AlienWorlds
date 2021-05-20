8591782be4ca00684017679020614eb2e479b6db
tomix89 <84495269+tomix89@users.noreply.github.com>
2021-05-20 22:34:16 +0200

    Add files via upload

--- listMinesOnLand/getMinesOnLand.js
+++ listMinesOnLand/getMinesOnLand.js
@@ -1,43 +1,78 @@
-let waxAddress = null;
-let landAddress = null;
+const zeroPad = (num, places) => String(num).padStart(places, '0')
 
-function printLine(line) {
-    console.log(line);
+let minerAddress = null;
+let landOwnerAddress = null;
+let landId = null;
+let resultField = null;
 
-    var p = document.createElement("p");
-    p.textContent = line;
-    document.body.appendChild(p);
-}
+let clearOnNext = false;
 
-function setMiner(address) {
-    waxAddress = address;
-    printLine("Miner is: " + waxAddress);
-}
-
-function setLandOwner(address) {
-    landAddress = address;
-    printLine("Land owner is: " + landAddress);
-}
 
 function checkMines() {
-    fetch('https://api.alienworlds.io/v1/alienworlds/mines?landowner=' + landAddress + '&miner=' + waxAddress + '&limit=999')
+
+    resultField = document.getElementById("resultField");
+
+    minerAddress = document.getElementById("minerName").value.trim();
+    landOwnerAddress = document.getElementById("landOwner").value.trim();
+    landId = document.getElementById("landId").value.trim();
+
+    console.log("minerAddress: ", minerAddress);
+    console.log("landOwnerAddress", landOwnerAddress);
+    console.log("landId", landId);
+
+    if (clearOnNext == true) {
+       resultField.textContent = "";
+    }
+
+    if ((landOwnerAddress != '') && (landId != '')) {
+        resultField.textContent += "Fill in EITHER Land owner OR Lanf ID\n"+
+			            "Alien Worlds API can't handle both in one request\n"          
+	clearOnNext = false;
+        return;
+    } else {
+        clearOnNext = true;
+    }
+
+    resultField.textContent = "Patience, running!";
+
+    let querry = "";
+    if (landOwnerAddress != '') {
+        querry = 'https://api.alienworlds.io/v1/alienworlds/mines?landowner=' + landOwnerAddress + '&miner=' + minerAddress + '&limit=5000';
+    } else {
+        querry = 'https://api.alienworlds.io/v1/alienworlds/mines?land_id=' + landId + '&miner=' + minerAddress + '&limit=5000';
+    }
+    console.log("querry", querry);
+
+    fetch(querry)
         .then(response => response.json())
         .then(json => {
             let TLMminedSum = 0;
+            let lines = "";
+
             if (json && json.results && json.results.length > 0) {
                 for (let i = 0; i < json.results.length; i++) {
                     let mined = json.results[i].bounty / 10000;
                     let time = json.results[i].block_timestamp;
-                    printLine(time.toString() + " -> " + mined.toFixed(4) + " TLM");
+
+                    line = "[" + zeroPad(i, 4) + "] | ";
+                    line += time.toString() + " | ";
+                    line += "minerAddress: " + json.results[i].miner + " | "
+                    line += "landId: " + json.results[i].land_id + " | "
+                    line += "landOwnerAddress: " + json.results[i].landowner + " | "
+                    line += mined.toFixed(4) + " TLM";
+                    lines += line + "\n";
                     TLMminedSum += mined;
                 }
 
-                printLine("Total mined on " + landAddress + " -> " + TLMminedSum.toString() + " TLM");
+                resultField.textContent = lines;
+
+                let totalMinedLabel = document.getElementById("totalMined");
+                totalMinedLabel.textContent = TLMminedSum.toString() + " TLM";
 
             } else {
-                printLine("Alien Wolds API is busy, try a few seconds later (reload - F5). Api responded: " + json);
+                resultField.textContent = "Alien Wolds API is busy (or error happened).\nDouble check your input and try a few seconds later (reload - F5). Api responded: " + json;
             }
         }).catch(error => {
-            console.error("ERR: ", error);
+            printLine("ERROR: ", error);
         });
 }
